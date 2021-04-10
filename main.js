a143ed8b2a02741aab52dfa111dbb97b788ed296
Dominik Brame <idominikbrame@gmail.com>
2021-04-10 12:17:50 -0500

    almost done

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-let users = [];
+let users = []
 let text = ""
 
 function fetchSomething() {
-  fetch('https://randomuser.me/api')
-    .then(response => {
-    return response.json()
+  fetch('https://randomuser.me/api/?results=5')
+    .then(response => response.json())
+    .then(data => data.results.forEach(item => users.push(item)))
+    console.log(users)
+}
+
+
+
+const displayUsers = () => {
+  let fetchList = document.getElementById("fetchList")
+  users.forEach(function(user) {
+    //Create name
+    let li = document.createElement("li")
+    li.innerText = user.name.last + ", " + user.name.first
+    //create image tag with attribute
+    let img = document.createElement("img")
+    img.setAttribute('src', user.picture.large)
+
+    //Create button
+    let btn = document.createElement("button")
+    btn.innerText = "Show more info"
+    btn.addEventListener('click', function(){
+      showMoreInfo(user, fetchList)
     })
-    .then(data => {
-      document.getElementById("fetchImage").src = data.results[0].picture.medium
-      document.getElementById("fetchName").innerHTML = data.results[0].name.last
-    })
-    
+
+    //Append list in DOM
+    fetchList.appendChild(li)
+    fetchList.appendChild(img)
+    fetchList.appendChild(btn)
+  })
+}
+
+const showMoreInfo = (user, fetchList) => {
+  let ul = document.createElement('ul')
+  let li = document.createElement('li')
+
+  li.innerText = `  Age: ${user.dob.age} 
+  Gender: ${user.gender}
+  Email: ${user.email}`
+  ul.appendChild(li)
+  console.log(ul)
 }
